1e6428dc944d7cb42b1df40ca7e52225851ef581
Jake Guzman <jacobguz26@gmail.com>
2022-08-31 05:59:29 -0700

    First commit for Ruby Basics Repo.

---
+++ until_loop_1.rb
@@ -0,0 +1,7 @@
+numbers = [7, 9, 13, 25, 18]
+condition = true
+
+until condition == false do
+	puts numbers
+	condition = false
+end
---
+++ while_loop_1.rb
@@ -0,0 +1,7 @@
+numbers = []
+
+while numbers[4] == nil
+	numbers << rand(0..100)
+end
+
+print numbers
