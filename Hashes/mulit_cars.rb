9bceba3a2303a0bbd02ced49f656e6ff2436dc31
Jake Guzman <jacobguz26@gmail.com>
2022-09-04 05:25:22 -0700

    Created Hash directory and completed exercises.

---
+++ Hashes/mulit_cars.rb
@@ -0,0 +1,12 @@
+multi_cars = {
+	car: {
+		type: "sedan",
+		color: "blue",
+		year: "2003",
+	},
+	truck: {
+		type: "pickup",
+		color: "red",
+		year: "1998",
+	}
+}
---
+++ Hashes/to_array.rb
@@ -0,0 +1,8 @@
+car = {
+  type:  'sedan',
+  color: 'blue',
+  year:  2003
+}
+
+array = car.to_a
+p array 
